e807b2aeb85cdfaf2336d20216e27aafaeffbe73
sbritos58 <s.britos@hotmail.com>
2020-06-05 01:59:13 +0200

    a ver si ahora si 7ads

--- StudioBrits/settings.py
+++ StudioBrits/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yeun=o3@4=4r&m+frc3&%1+6^!s&ct*1%5i+^d@k&lv@1p_*q_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -145,7 +145,3 @@ except ImportError:
     pass
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
-DEBUG = False
-
