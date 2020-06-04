c93006ad8805a32dc49257375f0982f200dbfedd
sbritos58 <s.britos@hotmail.com>
2020-06-05 01:51:19 +0200

    a ver si ahora si 7

--- StudioBrits/wsgi.py
+++ StudioBrits/wsgi.py
@@ -10,11 +10,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StudioBrits.settings')
 
 application = get_wsgi_application()
+
+from whitenoise import WhiteNoise
 
 application = WhiteNoise(application, root='/path/to/static/files')
 application.add_files('/path/to/more/static/files', prefix='more-files/')
