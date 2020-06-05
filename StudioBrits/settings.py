7c4ea74fddcaae902a8e9d5d69bffaef1c704223
sbritos58 <s.britos@hotmail.com>
2020-06-05 02:40:28 +0200

    a ver si ahora si 7ads121

--- StudioBrits/settings.py
+++ StudioBrits/settings.py
@@ -119,10 +119,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 STATIC_MEDIA = '/media/'
 
 EMAIL_USE_TLS = True
