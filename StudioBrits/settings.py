bc284d411d42e6daa2f98e2a354d0fe9e4e77511
sbritos58 <s.britos@hotmail.com>
2020-06-05 13:46:06 +0200

    a ver si ahora si 7ads121

--- StudioBrits/settings.py
+++ StudioBrits/settings.py
@@ -129,8 +129,7 @@ STATIC_MEDIA = '/media/'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST = "webmailsmtp.register.it"
+EMAIL_HOST = "authsmpt.securemail.pro"
 EMAIL_HOST_USER = "info@studiobrits.com"
 EMAIL_HOST_PASSWORD = "Penenayger5"
-EMAIL_PORT = 25
+EMAIL_PORT = 465
