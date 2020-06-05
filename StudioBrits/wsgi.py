9706d01ee4d3185561eee7c3f6afdac9b8ac21e6
sbritos58 <s.britos@hotmail.com>
2020-06-05 02:06:31 +0200

    a ver si ahora si 7ads121

--- StudioBrits/wsgi.py
+++ StudioBrits/wsgi.py
@@ -17,5 +17,4 @@ application = get_wsgi_application()
 
 from whitenoise import WhiteNoise
 
-application = WhiteNoise(application, root='/path/to/static/files')
-application.add_files('/path/to/more/static/files', prefix='more-files/')
+application = WhiteNoise(application)
